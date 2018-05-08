06e7481cbd6a7554d4d8527d34e9060cf10f84c9
Pranab Krishnan <proman1999@gmail.com>
2018-05-08 13:34:13 -0400

    turning to face color

--- takeoff.py
+++ takeoff.py
@@ -11,23 +11,17 @@ import sys
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
-# mavrosnew = autopilot.Mavros(mavros_prefix="/mavros2")
-# quadnew = core.Multirotor(mavrosnew, frequency=10)
-# quadnew.takeoff()
-# quadnew.set_position([100, 10, 10], blocking=True)
-#THIS IS THE UAV 2, USE IT FOR TESTING
 
-# rospy.sleep(3)
-# quadnew.land()
 
 
 class colorDetect:
 
-    def __init__(self):
+    def __init__(self,myQuad):
         self.image_pub = rospy.Publisher('/general_contours/image',Image,queue_size=10)
 
         self.bridge = CvBridge()
-        self.image_sub = rospy.Subscriber('/uav_1_camera_front/image_raw',Image,self.callback)#What is the image topic for uav?
+        self.image_sub = rospy.Subscriber('/uav_1_camera_down/image_raw',Image,self.callback)#What is the image topic for uav?
+        self.quad=myQuad
 
     def callback(self,data):
 
@@ -37,91 +31,114 @@ class colorDetect:
         except CvBridgeError as e:
             print(e)
 
-        # # circle drawing code
-        # (rows,cols,channels) = cv_image.shape
-        # if cols > 60 and rows > 60 :
-        #   cv2.circle(cv_image, (50,50), 10, 255)
-
+        #Guassian Blur
+        #cv_image = cv2.GaussianBlur(cv_image,(3,3),0)
 
         # Convert BGR to HSV
         hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
 
+        #white: (0,255)
 
-        # lower_blue = np.array([90,150,150])
-        # upper_blue = np.array([130,255,255])
+        lower_green=np.array([0,0,200])
+        upper_green=np.array([10,50,255])
 
-        ##    lower_orange=np.array([20, 80, 70])#30,92,81
-        ##    upper_orange=np.array([40,100,90])
-        #purple=np.uint8([[[128,0,128]]])
-
-        ##    print cv2.cvtColor(purple,cv2.COLOR_BGR2HSV)
-
-        # define range of purple color in HSV
-        ##    lower_purple=np.array([140, 200, 120])#150,255,128
-        ##    upper_purple=np.array([160,255,140])
-
-
-        lower_green=np.array([40,150,150])#hsv=60,255,255
-        upper_green=np.array([90,255,255])
 
         # Threshold the HSV image to get only green colors
-        # blue_mask = cv2.inRange(hsv, lower_blue, upper_blue)
+
         green_mask = cv2.inRange(hsv, lower_green, upper_green)
 
         # mask=blue_mask+green_mask
         mask=green_mask
-
-        # mask = cv2.erode(mask, None, iterations=2)
-        # mask = cv2.dilate(mask, None, iterations=2)
+        mask = cv2.erode(mask, None, iterations=2)
+        mask = cv2.dilate(mask, None, iterations=2)
 
 
         # Bitwise-AND mask and original image to give color as well to mask
         res = cv2.bitwise_and(cv_image,cv_image, mask= mask)
-
-        height, width, channels= cv_image.shape
-
-        #find centroid of "blobs"?
-        m=cv2.moments(mask, False)
-        try: 
-            cx, cy= m['m10']/m['m00'], m['01']/m['m00']#gets coords of mask
-
-        except ZeroDivisionError: #if no coords found
-            cx, cy=height/2, width/2
-
-
-        # print(cx, cy)
-
-        # cv2.imshow('frame',frame)
-        # cv2.imshow('mask',mask)#SHOW THIS IN GENERAL COUNTOURS IMAGE
-        # cv2.imshow('res',res)
-        # cv2.waitKey(0)
-
-        # cv2.imshow("Image window", cv_image)
         
 
         try:
-            self.image_pub.publish(self.bridge.cv2_to_imgmsg(mask, "bgr8"))#convert back to img message
+            self.image_pub.publish(self.bridge.cv2_to_imgmsg(res, "bgr8"))#convert back to img message
         except CvBridgeError as e:
             print(e)
 
     
+        height, width, channels= cv_image.shape
+        print("height: " + str(height))
+        print("width: " + str(width))
+        # print("channels: " +channels)
+        # cv2.imshow("Image window", cv_image)
+
+        #find centroid of "blobs"?
+        
+        m=cv2.moments(mask)
+        try: 
+            cx= int(m['m10']/m['m00'])
+            cy= int(m['m01']/m['m00'])#gets coords of mask
+
+        except ZeroDivisionError: #if no coords found
+            cy= int(height/2)
+            cx= int(width/2)
+            #continue on navigation
+
+        print(cx,cy)
+
+        vel_msg = quad.get_velocity()
+
+        #Enter zeroing in mode once color is found
+
+        while cx<280 or cx>360:
+            if quad.get_position().position.z >=20:
+                vel_msg.twist.linear.z =-1  
+            
+            if cx<280:
+                vel_msg.twist.angular.z=0.3
+                
+            else:
+                vel_msg.twist.angular.z=-0.3
+                
+            quad.set_velocity(vel_msg)
+
+        print("hello")
+
+        vel_msg.twist.linear.x=10
+        
+
+        # print(cx, cy)
+            
+        # quad.set_position([2, 2, 5], blocking=True)
+
 
 if __name__ == "__main__":
-    # rospy.init_node("node")
+    
     rospy.init_node('color_detector', anonymous=True)
     rate = rospy.Rate(10) # 10hz
     rate.sleep()
 
+    # mavrosnew = autopilot.Mavros(mavros_prefix="/mavros2")
+    # quadnew = core.Multirotor(mavrosnew, frequency=10)
+    # quadnew.takeoff()
+    # quadnew.set_position([-5, 5, 2], blocking=True)
+    # # THIS IS THE UAV 2, USE IT FOR TESTING
+    # rospy.sleep(3)
+    # try:
+    #     rospy.spin()
+        
+    # except KeyboardInterrupt:
+    #     print("Shutting down")
+        
+    # quadnew.land()
+
+
+    #Actual drone
     mavros = autopilot.Mavros(mavros_prefix="/mavros1")
     quad = core.Multirotor(mavros, frequency=10)
 
     quad.takeoff()
-    quad.set_position([25, 10, 10], blocking=True)
+    cd= colorDetect(quad)
+
+    quad.set_position([0, 0, 10], blocking=True)
     rospy.sleep(3)
-
-    # analyze
-
-    cd= colorDetect()
     
 
     try:
