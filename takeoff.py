7770180380f8729248691410649db83f453e2cee
Pranab Krishnan <proman1999@gmail.com>
2018-05-08 15:31:33 -0400

    commented

--- takeoff.py
+++ takeoff.py
@@ -80,7 +80,7 @@ class colorDetect:
         vel_msg = quad.get_velocity()
 
         #Enter zeroing in mode once color is found
-        while cx<280 or cx>360 or cy>240:
+        while cx<(width/2)-30 or cx>(width/2)+30 or cy>(height/2)-10: #while out of bounds
             # if quad.get_position().position.z >=20:
             #     vel_msg.twist.linear.z =-1 
             #WANT TO MAINTAIN CERTAIN HEIGHT 
@@ -92,6 +92,9 @@ class colorDetect:
                 vel_msg.twist.angular.z=-0.3
                 
             quad.set_velocity(vel_msg) #swivel until target is in front
+
+        #move toward target
+
 
         # vel_msg.twist.linear.x=10
             
@@ -141,76 +144,3 @@ if __name__ == "__main__":
     quad.land()
 
 
-
-
-
-
-# def callback(data):
-#     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-#     #analyze image here
-    
-# def analyze():
-
-#     # In ROS, nodes are uniquely named. If two nodes with the same
-#     # node are launched, the previous one is kicked off. The
-#     # anonymous=True flag means that rospy will choose a unique
-#     # name for our 'listener' node so that multiple listeners can
-#     # run simultaneously.
-#     rospy.init_node('analyzer', anonymous=True)
-
-#     rospy.Subscriber("chatter", String, callback)
-
-#     # spin() simply keeps python from exiting until this node is stopped
-#     rospy.spin()
-
-
-# cap = cv2.VideoCapture(0)#how do I subscribe to image node?
-
-# while(1):
-
-#     # Take each frame
-#     _, frame = cap.read()
-
-#     # Convert BGR to HSV
-#     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-    
-#     lower_blue = np.array([90,150,150])
-#     upper_blue = np.array([130,255,255])
-
-# ##    lower_orange=np.array([20, 80, 70])#30,92,81
-# ##    upper_orange=np.array([40,100,90])
-#     #purple=np.uint8([[[128,0,128]]])
-    
-# ##    print cv2.cvtColor(purple,cv2.COLOR_BGR2HSV)
-
-#     # define range of purple color in HSV
-# ##    lower_purple=np.array([140, 200, 120])#150,255,128
-# ##    upper_purple=np.array([160,255,140])
-
-    
-#     lower_green=np.array([40,150,150])#hsv=60,255,255
-#     upper_green=np.array([90,255,255])
-
-#     # Threshold the HSV image to get only blue colors
-#     blue_mask = cv2.inRange(hsv, lower_blue, upper_blue)
-#     green_mask = cv2.inRange(hsv, lower_green, upper_green)
-    
-#     mask=blue_mask+green_mask
-#     mask = cv2.erode(mask, None, iterations=2)
-#     mask = cv2.dilate(mask, None, iterations=2)
-    
-#     coords= cv2.findNonZero(mask)
-# ##    print coords
-
-#     # Bitwise-AND mask and original image to give color as well to mask
-#     res = cv2.bitwise_and(frame,frame, mask= mask)
-
-#     cv2.imshow('frame',frame)
-#     cv2.imshow('mask',mask)
-#     cv2.imshow('res',res)
-#     k = cv2.waitKey(5) & 0xFF
-#     if k == 27:
-#         break
-
-# cv2.destroyAllWindows()
